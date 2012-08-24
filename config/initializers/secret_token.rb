13318a1d536c62e816fe94fc84127c9014cb2e0e
vikas yaligar <vikasyaligar.it@gmail.com>
2012-08-24 23:40:56 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuesAns::Application.config.secret_token = 'e8c6006bbc74d6857755fe97e266b85043e3a9ade9c85e39dd6058fe9b90fd8d8810658fbe8fc9db64eea31cc3ce7a1daf6c76034cd7e7e9a75486eef5a1d8e0'
